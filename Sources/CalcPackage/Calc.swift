320eaea976039b3495b005f9e27bd1f6c15060f3
Idrish Sorathiya <idrish.sorathiya@rishabhsoft.com>
2023-12-30 15:10:06 +0530

    no message

---
+++ Sources/CalcPackage/Calc.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  
+//
+//  Created by Idrish Sorathiya on 30/12/23.
+//
+
+import Foundation
+
+public class Calc {
+    public init() {
+        
+    }
+    public func sum(a:Int, b:Int) -> Int {
+        return a + b
+    }
+}
